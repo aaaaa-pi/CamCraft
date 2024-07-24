28e15f84fa48967023a3de82156a1568f293845f
unknown <eureka77035@gmail.com>
2024-07-24 21:36:11 +0800

    feat:通过pinia插件实现持久化

--- src/renderer/src/stores/useConfigStore.ts
+++ src/renderer/src/stores/useConfigStore.ts
@@ -1,13 +1,21 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useConfigStore = defineStore('config', () => {
-  const config = ref({
-    deviceId: '',
-    page: 'camera'
-  })
+export const useConfigStore = defineStore(
+  'config',
+  () => {
+    const config = ref({
+      deviceId: '',
+      page: 'camera',
+      borderWidth: '10px',
+      borderColor: 'red'
+    })
 
-  const updateConfig = () => {}
+    const updateConfig = () => {}
 
-  return { config, updateConfig }
-})
+    return { config, updateConfig }
+  },
+  {
+    persist: true
+  }
+)
